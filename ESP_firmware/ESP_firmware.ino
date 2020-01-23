0410545bc4985cbb0ade8817dc59dffe281da2ea
lskjames <ptutm.jameslsk@gmail.com>
2020-01-23 13:04:10 +0800

    check in

--- ESP_firmware/ESP_firmware.ino
+++ ESP_firmware/ESP_firmware.ino
@@ -16,8 +16,10 @@ extern "C" {
 
 //#define WLAN_SSID       "Tenda_320CE0"
 //#define WLAN_PASS       "0105654525afk" //
-#define WLAN_SSID       "antlysis_meadow_2.4G@unifi"
-#define WLAN_PASS       "!Ath4ht@w4dt!"
+//#define WLAN_SSID       "antlysis_meadow_2.4G@unifi"
+//#define WLAN_PASS       "!Ath4ht@w4dt!"
+#define WLAN_SSID       "ideal 2nd floor"
+#define WLAN_PASS       "0379805027ideal"
 //#define WLAN_SSID "MX PJ21"
 //#define WLAN_PASS "ssot1178"
 //#define WLAN_SSID "MX SP"
@@ -37,8 +39,8 @@ extern "C" {
 #define IPSO_W_IN 10    //JP8
 #define IPSO_W_STATUS 14   //JP1
 #define IPSO_W_CTRL 12     //JP2
-#define DTG_CA_CTRL 5     //JP6
-#define DTG_CA1_IN 10       //JP8
+#define DTG_CA_CTRL 2     //JP6
+#define DTG_CA1_IN 5       //JP8
 #define DTG_CA2_IN 12      //JP2
 #define DTG_MTR_BEG 14     //JP1 (first two)
 #define DTG_MTR_SFT 16      //JP3 (second two)
@@ -54,12 +56,15 @@ extern "C" {
 #define DEX_D_CTRL  2      //JP4
 #define DEX_W_CTRL  2      //JP4     
 
+#define SHIFT_IN_DATA 14     //JP1 (first two)
+#define SHIFT_IN_LATCH 4      //JP3 (second two)
+#define SHIFT_IN_CLOCK 16      //JP5 (third two)
 
 /////////////////////////////////
 ////// MQTT SETUP ///////////////
 /////////////////////////////////
 
-#define MQTT_SERVER      "192.168.1.3" // give static address
+#define MQTT_SERVER      "192.168.0.9" // give static address
 #define MQTT_PORT         1883
 #define MQTT_USERNAME    ""
 #define MQTT_PASSWORD         ""
@@ -69,36 +74,44 @@ extern "C" {
 /////////////////////////////////
 
 const int FW_VERSION = 9;
-const char* fwUrlBase = "http://192.168.1.3/fw/";
+const char* fwUrlBase = "http://192.168.0.9/fw/";
 
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 WiFiClient client;
 //Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
 Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_PORT, MQTT_USERNAME, MQTT_PASSWORD);
-Adafruit_MQTT_Publish lastwill = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME "connectivity/467b712f6f3dd1baad34f7b11b75f7d1");
+Adafruit_MQTT_Publish lastwill = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME "connectivity/e15ceae8fa62a160cebe0a1644e6116c");
 
 // Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-Adafruit_MQTT_Publish runStatus = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME "Lock/467b712f6f3dd1baad34f7b11b75f7d1");
-Adafruit_MQTT_Publish detStatus = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME "detDrop/467b712f6f3dd1baad34f7b11b75f7d1");
-Adafruit_MQTT_Publish coinIn = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME "coinIn/467b712f6f3dd1baad34f7b11b75f7d1");
-Adafruit_MQTT_Publish versFeed = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME "versionFeed/467b712f6f3dd1baad34f7b11b75f7d1");
+Adafruit_MQTT_Publish runStatus = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME "Lock/e15ceae8fa62a160cebe0a1644e6116c");
+Adafruit_MQTT_Publish detStatus = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME "detDrop/e15ceae8fa62a160cebe0a1644e6116c");
+Adafruit_MQTT_Publish coinIn = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME "coinIn/e15ceae8fa62a160cebe0a1644e6116c");
+Adafruit_MQTT_Publish versFeed = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME "versionFeed/e15ceae8fa62a160cebe0a1644e6116c");
 // Setup a feed called 'esp8266_led' for subscribing to changes.
-Adafruit_MQTT_Subscribe receivedPayment = Adafruit_MQTT_Subscribe(&mqtt, MQTT_USERNAME "467b712f6f3dd1baad34f7b11b75f7d1");
+Adafruit_MQTT_Subscribe receivedPayment = Adafruit_MQTT_Subscribe(&mqtt, MQTT_USERNAME "e15ceae8fa62a160cebe0a1644e6116c");
 Adafruit_MQTT_Subscribe firmwareUpdate = Adafruit_MQTT_Subscribe(&mqtt, MQTT_USERNAME "firmwareUpdate");
 
 ////////////////////////////////////
 //////// SOME VARIABLE INIT ////////
 ////////////////////////////////////
 
-int sftnrCounter = 0;
-int sftnrDrop = 0;
-int turn_sft = 0;
-int dtgCounter = 0;
-int dtgDrop = 0;
-int turn_dtg = 0;
-int begCounter = 0;
-int begDrop = 0;
-int turn_beg = 0;
+unsigned int sftnrCounter = 0;
+unsigned int sftnrDrop = 0;
+unsigned int turn_sft = 0;
+unsigned int dtgCounter1 = 0;
+unsigned int dtgCounter2 = 0;
+unsigned int dtgDrop = 0;
+unsigned int turn_dtg1 = 0;
+unsigned int turn_dtg2 = 0;
+unsigned int dsCounter1 = 0;
+unsigned int dsDrop = 0;
+unsigned int turn_drySoft = 0;
+unsigned int blchCounter = 0;
+unsigned int blchDrop = 0;
+unsigned int turn_blch2 = 0;
+unsigned int begCounter = 0;
+unsigned int begDrop = 0;
+unsigned int turn_beg = 0;
 int coin_input1 = 0;
 int coin_input2 = 0;
 int bill_input = 0;
@@ -121,10 +134,12 @@ int SendOne1 = 0;
 int checkOne2 = 0;
 int SendOne2 = 0;
 int start = 0;
+boolean cneted = true;
 unsigned long previousMillis = 0;
 const long interval = 5000;
+unsigned int register_value = 0;
 // ipsoWasher | ipsoDryer | dexWasher | dexDryerSingle | dexDryerDouble | detergent | billAcceptor
-String machineType = String("ipsoWasher");
+String machineType = String("detergent");
 
 ////////////////////////////////////
 //////// TIME AND INTERRUPT ////////
@@ -132,6 +147,7 @@ String machineType = String("ipsoWasher");
 // start the timer for the heartbeat
 os_timer_t myHeartBeat;
 os_timer_t IOReadout;
+os_timer_t ShiftRegister;
 
 void timerCallback(void *pArg) {
   //////Serial.print("timer is functioning\b");
@@ -142,6 +158,7 @@ void IOtimerISR(void *pArg) {
   if (machineType == "ipsoWasher") {
     lockState1 =  digitalRead(IPSO_W_STATUS);
     coin_input1 = digitalRead(IPSO_W_IN);
+    coinCount2(coin_input1, &coinCounter1, &high1, &start);
   } else if (machineType == "ipsoDryer") {
     lockState1 =  digitalRead(IPSO_D_STATUS);
     coin_input1 = digitalRead(IPSO_D_IN);
@@ -155,16 +172,41 @@ void IOtimerISR(void *pArg) {
     lockState1 =  digitalRead(DEX_D1_STATUS);
     lockState2 =  digitalRead(DEX_D2_STATUS);
     coin_input1 = digitalRead(DEX_D_IN);
+    coinCount(coin_input1, &coinCounter1, &high1, &start);
   }
-  coinCount(coin_input1, &coinCounter1, &high1, &start);
 }
 
-//void ShiftRegisterISR(void *pArg) {
-//   /* Read in first 74HC165 data. */
-//   register_value = shift_in();
-//   Serial.print(register_value, BIN);
-//   Serial.println(" ");
-//}
+unsigned int shift_in(void) {
+  unsigned int byte = 0;
+  unsigned int pin_value;
+
+  digitalWrite(SHIFT_IN_LATCH, LOW);
+  delayMicroseconds(200);
+  digitalWrite(SHIFT_IN_LATCH, HIGH);
+  delayMicroseconds(200);
+
+  for(int i=0; i<8; i++) {
+      pin_value = digitalRead(SHIFT_IN_DATA);  
+      byte |= (pin_value << ((8 - 1) - i));
+
+//      Serial.print(pin_value, BIN);
+//      Serial.println(" ");
+//      delay(1000);
+      /* Pulse clock to write next bit. */
+      digitalWrite(SHIFT_IN_CLOCK, LOW);
+      delayMicroseconds(500);
+      digitalWrite(SHIFT_IN_CLOCK, HIGH);
+      delayMicroseconds(500);
+  }
+    return byte;
+}
+
+void ShiftRegisterISR(void *pArg) {
+   /* Read in first 74HC165 data. */
+   register_value = shift_in();
+   Serial.print(register_value, BIN);
+   Serial.println(" ");
+}
 
 void timerInit(void) {
   os_timer_setfn(&myHeartBeat, timerCallback, NULL);
@@ -173,8 +215,8 @@ void timerInit(void) {
   os_timer_setfn(&IOReadout, IOtimerISR, NULL);
   os_timer_arm(&IOReadout, 5, true);
 
-  //os_timer_setfn(&ShiftRegister, ShiftRegisterISR, NULL);
-  //os_timer_arm(&ShiftRegister, 15, true);
+  os_timer_setfn(&ShiftRegister, ShiftRegisterISR, NULL);
+  os_timer_arm(&ShiftRegister, 15, true);
 }
 
 
@@ -229,9 +271,11 @@ void setup() {
     pinMode(DTG_CA1_IN, INPUT);
     pinMode(DTG_CA_CTRL, OUTPUT);
     pinMode(DTG_CA2_IN, INPUT);
-    pinMode(DTG_MTR_SFT, INPUT);
-    pinMode(DTG_MTR_DTG, INPUT);
-    pinMode(DTG_MTR_BEG, INPUT);
+    pinMode(SHIFT_IN_CLOCK, OUTPUT);
+    pinMode(SHIFT_IN_LATCH, OUTPUT);
+    pinMode(SHIFT_IN_DATA, INPUT);
+    digitalWrite(SHIFT_IN_LATCH, HIGH);
+    digitalWrite(SHIFT_IN_CLOCK, HIGH);
     digitalWrite(DTG_CA_CTRL, LOW);
     //delay(2000);
     //digitalWrite(DTG_CA_CTRL,LOW);
@@ -257,7 +301,7 @@ void setup() {
   //////Serial.println("The firmware has changed");
   mqtt.subscribe(&receivedPayment);
   mqtt.subscribe(&firmwareUpdate);
-  mqtt.will(MQTT_USERNAME "connectivity/467b712f6f3dd1baad34f7b11b75f7d1", "OFF");
+  mqtt.will(MQTT_USERNAME "connectivity/e15ceae8fa62a160cebe0a1644e6116c", "OFF");
   timerInit();
   versFeed.publish(FW_VERSION);
   //payDet_NO(9);
@@ -274,8 +318,8 @@ String mac;
 void loop() {
   // Ensure the connection to the MQTT server is alive (this will make the first
   // connection and automatically reconnect when disconnected).  See the MQTT_connect
-  MQTT_connect();
-  if (!donefw) {
+  MQTT_connect(&cneted);
+  if (!donefw && cneted) {
     versFeed.publish(FW_VERSION);
     donefw = true;
     mac = getMAC();
@@ -408,14 +452,11 @@ void loop() {
       }
     }
   } else if (machineType == "detergent") {
-    sftnrDrop =  digitalRead(DTG_MTR_SFT);
-    dtgDrop = digitalRead(DTG_MTR_DTG);
-    begDrop = digitalRead(DTG_MTR_BEG);
     coin_input1 = digitalRead(DTG_CA1_IN);
     coin_input2 = digitalRead(DTG_CA2_IN);
     //bill_input = digitalRead(BILL_IN);
     detergentCoinCount(coin_input1, coin_input2, &high1, &high2, &coinCounter1, &coinCounter2);
-    detergentReadStatus(&turn_sft, &turn_beg, &turn_dtg, sftnrDrop, dtgDrop, begDrop, &sftnrCounter, &begCounter, &dtgCounter);
+    DetReadStatus(&register_value, &turn_sft, &turn_beg, &turn_drySoft, &turn_blch2,  &turn_dtg1, &turn_dtg2, &sftnrCounter, &begCounter, &dtgCounter1, &dtgCounter2, &dsCounter1, &blchCounter);
     //billInRead(bill_input, &billhigh1, &billCounter);
     while ((subscription = mqtt.readSubscription())) {
       if (subscription == &receivedPayment) {
@@ -467,15 +508,17 @@ void loop() {
 ////// FUNCTIONS /////////////////
 //////////////////////////////////
 
-void MQTT_connect() {
+void MQTT_connect(boolean *cneted) {
   int8_t ret;
   int mycount;
   mycount = 0;
   // Stop if already connected.
   if (mqtt.connected()) {
+    *cneted = true;
     ////Serial.println("MQTT Connected!");
     return;
   }
+  *cneted = false;
   unsigned long currentMillis = millis();
   // Serial.print("Connecting to MQTT... ");
   if (currentMillis - previousMillis >= interval) {
@@ -486,7 +529,7 @@ void MQTT_connect() {
       previousMillis = currentMillis;
     }
   }
-
+  //Serial.println("MQTT Connected!");
 }
 
 void payIpsoW_NO(int amt) {
@@ -611,13 +654,46 @@ void coinCount(int coin_input, int *coinHigh, int *high, int *coinLow) {
         *high = 1;
       }
     } else {
-      if (*coinLow > 100) {
+      if (*coinLow > 30) {
         *coinHigh = 0;
       }
       *high = 0;
     }
   } else if (*high == 1) {
     if (coin_input == LOW) {
+      //Serial.print("coin low");
+      (*coinLow)++;
+      //Serial.println(*coinLow);
+      if (*coinLow > 30) {
+        *coinHigh = 0;
+        *coinLow = 0;
+        *high = 0;
+        coinIn.publish("1COIN");
+      }
+    } else {
+      *high = 1;
+    }
+  }
+}
+
+void coinCount2(int coin_input, int *coinHigh, int *high, int *coinLow) {
+  if (*high == 0) {
+    if (coin_input == LOW) {
+      //Serial.print("coin high");
+      (*coinHigh)++;
+      //Serial.println(*coinHigh);
+      if (*coinHigh > 3) {
+        *coinHigh = 0;
+        *high = 1;
+      }
+    } else {
+      if (*coinLow > 100) {
+        *coinHigh = 0;
+      }
+      *high = 0;
+    }
+  } else if (*high == 1) {
+    if (coin_input == HIGH) {
       //Serial.print("coin low");
       (*coinLow)++;
       //Serial.println(*coinLow);
@@ -858,7 +934,7 @@ void detergentCoinCount(int coin_input1, int coin_input2, int *high1, int *high2
   ///////////////////////////////////
 
   if (*high1 == 0) {
-    if (coin_input1 == HIGH) {
+    if (coin_input1 == LOW) {
       (*coinCounter1)++;
       if (*coinCounter1 > 3) {
         *high1 = 1;
@@ -870,7 +946,7 @@ void detergentCoinCount(int coin_input1, int coin_input2, int *high1, int *high2
       *high1 = 0;
     }
   } else if (*high1 == 1) {
-    if (coin_input1 == LOW) {
+    if (coin_input1 == HIGH) {
       (*coinCounter1)++;
       if (*coinCounter1 > 10) {
         *high1 = 0;
@@ -888,7 +964,7 @@ void detergentCoinCount(int coin_input1, int coin_input2, int *high1, int *high2
 
 
   if (*high2 == 0) {
-    if (coin_input2 == HIGH) {
+    if (coin_input2 == LOW) {
       (*coinCounter2)++;
       if (*coinCounter2 > 3) {
         *high2 = 1;
@@ -900,7 +976,7 @@ void detergentCoinCount(int coin_input1, int coin_input2, int *high1, int *high2
       *high2 = 0;
     }
   } else if (*high2 == 1) {
-    if (coin_input2 == LOW) {
+    if (coin_input2 == HIGH) {
       (*coinCounter2)++;
       if (*coinCounter2 > 10) {
         *high2 = 0;
@@ -909,6 +985,160 @@ void detergentCoinCount(int coin_input1, int coin_input2, int *high1, int *high2
       }
     } else {
       *high2 = 1;
+    }
+  }
+}
+
+void DetReadStatus(unsigned int *shiftReg, unsigned int *turn_sft, unsigned int *turn_beg, unsigned int *turn_drySoft, unsigned int *turn_bleach,  unsigned int *turn_dtg1, unsigned int *turn_dtg2, unsigned int *sftnrCounter, unsigned int *begCounter, unsigned int *dtgCounter1, unsigned int *dtgCounter2, unsigned int *dsCounter1, unsigned int *blchCounter) {
+  unsigned int compSoft =            *shiftReg & 128;
+  unsigned int compOxyBleach =       *shiftReg & 64; //done
+  unsigned int compDrySoft =         *shiftReg & 32; //done
+  unsigned int compLB =               *shiftReg & 16; //done
+  unsigned int compDet1 =            *shiftReg & 8; //done
+  unsigned int compDet2 =              *shiftReg & 4;
+  //unsigned int compbill =      *shiftReg & 0b00000010;
+  //delay(1000);
+  //// Compare first slot for Det 1 ////////
+  //detStatus.publish(*shiftReg);
+  if (*turn_dtg1 == 0) {
+    if (compDet1 == 8) {
+      (*dtgCounter1)++;
+      if (*dtgCounter1 > 3) {
+        *turn_dtg1 = 1;
+        *dtgCounter1 = 0;
+      }
+    } else {
+      *turn_dtg1 = 0;
+    }
+  } else if (*turn_dtg1 == 1) {
+    if (compDet1 == 0) {
+      (*dtgCounter1)++;
+      if (*dtgCounter1 >20) {
+        *turn_dtg1 = 0;
+        detStatus.publish("DTG_DROP1");
+      } 
+    } else {
+      *turn_dtg1 = 1;
+    }
+  }
+
+//// Compare second slot for Det 2 ////////
+  
+  if (*turn_dtg2 == 0) {
+    if (compDet2 == 4) {
+      (*dtgCounter2)++;
+      if (*dtgCounter2 > 3) {
+        *turn_dtg2 = 1;
+        *dtgCounter2 = 0;
+      }
+    } else {
+      *turn_dtg2 = 0;
+    }
+  } else if (*turn_dtg2 == 1) {
+    if (compDet2 == 0) {
+      (*dtgCounter2)++;
+      if (*dtgCounter2 >20) {
+        *turn_dtg2 = 0;
+        detStatus.publish("DTG_DROP2");
+      } 
+    } else {
+      *turn_dtg2 = 1;
+    }
+  }
+
+//// Compare third slot for Softener ////////
+
+  if (*turn_sft == 0) {
+    if (compSoft == 128) {
+      (*sftnrCounter)++;
+      if (*sftnrCounter > 3) {
+        *turn_sft = 1;
+        *sftnrCounter = 0;
+      }
+    } else {
+      *turn_sft = 0;
+    }
+  } else if (*turn_sft == 1) {
+    if (compSoft == 0) {
+      (*sftnrCounter)++;
+      if (*sftnrCounter >20) {
+        *turn_sft = 0;
+        detStatus.publish("SFT_DROP");
+      } 
+    } else {
+      *turn_sft = 1;
+    }
+  }
+  
+//// Compare 4th slot for Bleach 1 ////////
+  
+  if (*turn_drySoft == 0) {
+    if (compDrySoft == 32) {
+      (*dsCounter1)++;
+      if (*dsCounter1 > 3) {
+        *turn_drySoft = 1;
+        *dsCounter1 = 0;
+      }
+    } else {
+      *turn_drySoft = 0;
+    }
+  } else if (*turn_drySoft == 1) {
+    if (compDrySoft == 0) {
+      (*dsCounter1)++;
+      if (*dsCounter1 >20) {
+        *turn_drySoft = 0;
+        detStatus.publish("DrYSOFT_DROP");
+      } 
+    } else {
+      *turn_drySoft = 1;
+    }
+  }
+
+//// Compare 5th slot for Bleach 2 ////////
+  
+  if (*turn_bleach == 0) {
+    if (compOxyBleach == 64) {
+      (*blchCounter)++;
+      if (*blchCounter > 3) {
+        *turn_bleach = 1;
+        *blchCounter = 0;
+      }
+    } else {
+      *turn_bleach = 0;
+    }
+  } else if (*turn_bleach == 1) {
+    if (compOxyBleach == 0) {
+      (*blchCounter)++;
+      if (*blchCounter >20) {
+        *turn_bleach = 0;
+        detStatus.publish("OXY_DROP"); 
+      } 
+    } else {
+      *turn_bleach = 1;
+    }
+  }
+
+//// Compare 6th slot for LB ////////
+
+  if (*turn_beg == 0) {
+    if (compLB == 16) {
+      (*begCounter)++;
+      if (*begCounter > 3) {
+        *turn_beg = 1;
+        *begCounter = 0;
+      }
+    } else {
+      *turn_beg = 0;
+    }
+  } else if (*turn_beg == 1) {
+    if (compLB == 0) {
+      (*begCounter)++;
+      if (*begCounter >20) {
+        *turn_beg = 0;
+        detStatus.publish("BEG_DROP");
+      } 
+    } else {
+      *turn_beg = 1;
     }
   }
 }
@@ -1010,7 +1240,3 @@ boolean isValidNumber(String str) {
   }
   return isNum;
 }
-
-
-
-
